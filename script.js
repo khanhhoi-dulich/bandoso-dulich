36cf5d0c0b136698193bdb53378a59747ec71c3f
khanhhoi-dulich <vhxh.khcn@gmail.com>
2026-05-05 08:18:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,6 +33,20 @@ fetch('./Phuong.geojson')
     console.error("Lỗi load GEOJSON:", err);
 });
 var locations = [
+   {
+        name: "Văn phòng Đảng ủy phường Khánh Hội",
+        lat: 10.760295,
+        lng: 106.700214,
+        desc: "31 đường 12A, Phường Khánh Hội, TP. Hồ Chí Minh",
+        type: "hanhchinh"
+    },
+    {
+        name: "Ủy ban Mặt trận Tổ quốc Việt Nam phường Khánh Hội",
+        lat: 10.760295,
+        lng: 106.700214,
+        desc: "31 đường 12A, Phường Khánh Hội, TP. Hồ Chí Minh",
+        type: "hanhchinh"
+    },
     {
         name: "Văn phòng HĐND-UBND Phường",
         lat: 10.764222,
@@ -45,6 +59,13 @@ var locations = [
         lat: 10.761353,
         lng: 106.705483,
         desc: "531 Vĩnh Khánh, phường Khánh Hội, TP. HCM",
+        type: "hanhchinh"
+    }
+      {
+        name: "Trạm Y tế phường Khánh Hội",
+        lat: 10.76179,
+        lng: 106.69779,
+        desc: "178 Bến Vân Đồn, phường Khánh Hội, TP. HCM",
         type: "hanhchinh"
     }
 ];
