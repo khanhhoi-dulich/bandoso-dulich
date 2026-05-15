54342a228eb8a336f447ca0f611867db720f170b
khanhhoi-dulich <vhxh.khcn@gmail.com>
2026-05-15 09:34:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -71,6 +71,13 @@ var locations = [
         type: "hanhchinh"
     },
       {
+       name: "Trung tâm cung ứng dịch vụ công",
+       lat: 10.759868,
+       long: 106.701809,
+       desc: 129F/64A, Bến Vân Đồn, phường Khánh Hội, TP. Hồ Chí Minh",
+       type: "hanhchinh"
+    };
+      {
         name: "Trạm Y tế Phường Khánh Hội",
         lat: 10.76179,
         lng: 106.69779,
